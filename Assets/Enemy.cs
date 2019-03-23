683f0a03c16599f8d509fb922052bba3dc735c04
Ryan Zubery <zubery@usc.edu>
2019-03-23 14:41:08 -0700

    Prototype continous vs discrete motion

---
+++ Assets/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    float timer;
+    Rigidbody rb; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0.0f;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        float force = 1000.0f;
+
+        if (timer >= 1.0f)
+        {
+            int direction = Random.Range(1, 4);
+
+            if (direction == 1)
+            {
+                rb.AddForce(Vector3.right * force);
+            }
+            else if (direction == 2)
+            {
+                rb.AddForce(Vector3.left * force);
+            }
+            else if (direction == 3)
+            {
+                rb.AddForce(Vector3.forward * force);
+            }
+            else if (direction == 4)
+            {
+                rb.AddForce(Vector3.back * force);
+            }
+            timer = 0.0f;
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float timer;
+    Rigidbody rb;
+    float force = 2000.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0.0f;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        rb.AddForce(Vector3.back * (50f)); 
+
+        if(timer >= 0.5f)
+        {
+            timer = 0.0f;
+
+            if (Input.GetKey(KeyCode.A)) {
+                rb.AddForce(Vector3.left * force);
+            }
+            else if (Input.GetKey(KeyCode.D)) {
+                rb.AddForce(Vector3.right * force);
+            }
+            else if (Input.GetKey(KeyCode.W)) {
+                rb.AddForce(Vector3.forward * force);
+            }
+            else if (Input.GetKey(KeyCode.S)) {
+                rb.AddForce(Vector3.back * force);
+            }
+        }
+    }
+}
